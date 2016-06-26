5c28ed4206d7f95ce3bd6fce25bf5e733b11941c
AJ <avjmail@gmail.com>
2016-06-26 14:04:55 +0200

    crossfilter tutorial

---
+++ javascripts/dashboard.js
@@ -0,0 +1,9 @@
+var livingThings = crossfilter([
+  // Fact data.
+  { name: “Rusty”,  type: “human”, legs: 2 },
+  { name: “Alex”,   type: “human”, legs: 2 },
+  { name: “Lassie”, type: “dog”,   legs: 4 },
+  { name: “Spot”,   type: “dog”,   legs: 4 },
+  { name: “Polly”,  type: “bird”,  legs: 2 },
+  { name: “Fiona”,  type: “plant”, legs: 0 }
+]);
