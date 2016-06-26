db675b11da88816f2881fd7467d156431fb003a1
AJ <avjmail@gmail.com>
2016-06-26 20:57:18 +0200

    cleanup and comments

--- javascripts/dashboard.js
+++ javascripts/dashboard.js
@@ -1,5 +1,6 @@
 var data = [];
 
+// current workaround for multiple data loading 
 d3.json('https://health.data.ny.gov/resource/s8d9-z734.json', function(error, data2009){
 d3.json('https://health.data.ny.gov/resource/dpew-wqcg.json', function(error, data2010){
 d3.json('https://health.data.ny.gov/resource/n5y9-zanf.json', function(error, data2011){
@@ -7,6 +8,7 @@ d3.json('https://health.data.ny.gov/resource/rv8x-4fm3.json', function(error, da
 d3.json('https://health.data.ny.gov/resource/tdf6-7fpk.json', function(error, data2013){
 d3.json('https://health.data.ny.gov/resource/pzzw-8zdv.json', function(error, data2014){
 
+    // concatenate all data into one 
     data = data.concat(data2009);
     data = data.concat(data2010);
     data = data.concat(data2011);
@@ -14,40 +16,39 @@ d3.json('https://health.data.ny.gov/resource/pzzw-8zdv.json', function(error, da
     data = data.concat(data2013);
     data = data.concat(data2014);
 
-    var yearFormat = d3.time.format('%Y'),
-        dayOfWeekFormat = d3.time.format('%a');
-
-
+    // data cleanup 
+    var yearFormat = d3.time.format('%Y');
+    var dayOfWeekFormat = d3.time.format('%a');
     _.each(data, function(d) {
         d.year = yearFormat(new Date(d.discharge_year));
         d.day = dayOfWeekFormat(new Date(d.discharge_day_of_week));
         d.length_of_stay = +d.length_of_stay;
     });
 
+    // main magic!
     var ndx = crossfilter(data);
 
-    var yearDim = ndx.dimension(dc.pluck('year')),
-        dayDim = ndx.dimension(dc.pluck('discharge_day_of_week')),
-        stayDim = ndx.dimension(dc.pluck('length_of_stay')); 
+    // Below follows lot of controls.
+    // Each code block creates: dimension, measures (counts per dimensions), chart init and configuration, reset button
 
-    var all = ndx.groupAll();
-
-    var countPerYear = yearDim.group().reduceCount(),
-        countPerDay = dayDim.group().reduceCount(),
-        countPerStay = stayDim.group().reduceCount();
-
-    var yearChart = dc.pieChart('#chart-ring-year'),   
-        dayChart = dc.pieChart('#chart-ring-day'),   
-        stayChart = dc.barChart('#chart-stay-count'),
-        dataCount = dc.dataCount('#data-count');   
-    
+    //---------------------------------------------------------- years
+    var yearDim = ndx.dimension(dc.pluck('year'));
+    var countPerYear = yearDim.group().reduceCount();
+    var yearChart = dc.pieChart('#chart-ring-year');   
     yearChart
         .width(150)
         .height(150)
         .dimension(yearDim)
         .group(countPerYear)
         .innerRadius(20);
-
+    d3.selectAll('a#resetYear').on('click', function () {
+        yearChart.filterAll();
+        dc.redrawAll();
+    });
+    //---------------------------------------------------------- day of week
+    var dayDim = ndx.dimension(dc.pluck('discharge_day_of_week'));
+    var countPerDay = dayDim.group().reduceCount();
+    var dayChart = dc.pieChart('#chart-ring-day');  
     dayChart
         .width(150)
         .height(150)
@@ -62,7 +63,15 @@ d3.json('https://health.data.ny.gov/resource/pzzw-8zdv.json', function(error, da
             return order[d.key];
             }
         );
-
+    d3.selectAll('a#resetWeekday').on('click', function () {
+        dayChart.filterAll();
+        dc.redrawAll();
+    });
+    //---------------------------------------------------------- length of stay
+    var stayDim = ndx.dimension(dc.pluck('length_of_stay')); 
+    var countPerStay = stayDim.group().reduceCount();
+    var stayChart = dc.barChart('#chart-stay-count'),
+        dataCount = dc.dataCount('#data-count');   
     stayChart
         .width(300)
         .height(180)
@@ -75,32 +84,21 @@ d3.json('https://health.data.ny.gov/resource/pzzw-8zdv.json', function(error, da
         .xAxisLabel('Days of stay')
         .yAxisLabel('Count')
         .margins({top: 10, right: 20, bottom: 50, left: 50});
-
     stayChart.xAxis().tickValues([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-
-    dataCount
-        .dimension(ndx)
-        .group(all);
-
-    d3.selectAll('a#resetAll').on('click', function () {
-        dc.filterAll();
-        dc.renderAll();
-    });
-
-    d3.selectAll('a#resetYear').on('click', function () {
-        yearChart.filterAll();
-        dc.redrawAll();
-    });
-
-    d3.selectAll('a#resetWeekday').on('click', function () {
-        dayChart.filterAll();
-        dc.redrawAll();
-    });
-
     d3.selectAll('a#resetStay').on('click', function () {
         stayChart.filterAll();
         dc.redrawAll();
     });
-
+    // count widget (count all records and selected)
+    var all = ndx.groupAll();
+    dataCount
+        .dimension(ndx)
+        .group(all);
+    // reset all selections
+    d3.selectAll('a#resetAll').on('click', function () {
+        dc.filterAll();
+        dc.renderAll();
+    });
+    // render all the things!
     dc.renderAll();
 })})})})})});
