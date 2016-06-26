2ef8ff07ad31f5475a6942464ad508d1f2e5e92f
AJ <avjmail@gmail.com>
2016-06-26 16:41:49 +0200

    year chart

--- javascripts/dashboard.js
+++ javascripts/dashboard.js
@@ -1,9 +1,28 @@
-var livingThings = crossfilter([
-  // Fact data.
-  { name: “Rusty”,  type: “human”, legs: 2 },
-  { name: “Alex”,   type: “human”, legs: 2 },
-  { name: “Lassie”, type: “dog”,   legs: 4 },
-  { name: “Spot”,   type: “dog”,   legs: 4 },
-  { name: “Polly”,  type: “bird”,  legs: 2 },
-  { name: “Fiona”,  type: “plant”, legs: 0 }
-]);
+d3.json('../data.json', function(error, data){
+ 
+    var yearFormat = d3.time.format('%Y');
+
+    _.each(data, function(d) {
+        d.dischargeYear = yearFormat(new Date(d.discharge_year));
+    });
+
+    var ndx = crossfilter(data);
+
+    var yearDim  = ndx.dimension(function(d) {return d.dischargeYear;});
+
+    var all = ndx.groupAll();
+
+    var countPerYear = yearDim.group().reduceCount();
+
+    var yearChart   = dc.pieChart('#chart-ring-year');   
+    
+    yearChart
+        .width(150)
+        .height(150)
+        .dimension(yearDim)
+        .group(countPerYear)
+        .innerRadius(20);
+
+    
+    dc.renderAll();
+});
