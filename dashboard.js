35635117d682c729d75149f05994474a05690e08
AJ <avjmail@gmail.com>
2016-08-10 08:29:42 +0300

    using Promise - proof of concept

--- dashboard.js
+++ dashboard.js
@@ -24,6 +24,44 @@ function getDataUrl(year){
 }
 var randomOffsets = {};
 
+
+function get(url) {
+  // Return a new promise.
+  return new Promise(function(resolve, reject) {
+    // Do the usual XHR stuff
+    var req = new XMLHttpRequest();
+    req.open('GET', url);
+
+    req.onload = function() {
+      // This is called even on 404 etc so check the status
+      if (req.status == 200) {
+        // Resolve the promise with the response text
+        resolve(req.response);
+      }
+      else {
+        // Otherwise reject with the status text which will hopefully be a meaningful error
+        reject(Error(req.statusText));
+      }
+    };
+
+    // Handle network errors
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    // Make the request
+    req.send();
+  });
+}
+
+function getJSON (url) {
+    return get(url).then(JSON.parse);
+}
+
+getJSON(getCountUrl(2009)).then(function(response) {
+  console.log(response[0].count);
+})
+
 d3.json(getCountUrl(2009), function(err, counted2009){
     randomOffsets[2009] = getRandom(counted2009);
 d3.json(getCountUrl(2010), function(err, counted2010){
